0e479c7b84df1c0f4c0fe7d9eb24bed720f947d0
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2024-08-15 13:29:31 +0100

    upload all files

---
+++ JSmyscript.js
@@ -0,0 +1,20 @@
+function submit() {
+  const paragraph = document.getElementById("message");
+  paragraph.innerHTML = "You're signed up!";
+};
+
+function groupCyc() {
+  window.open("/Pages/Group Cycling.html")
+}
+
+function coach() {
+  window.open("/Pages/Coaching.html")
+}
+
+function mountRide() {
+  window.open("/Pages/Mountain Rides.html")
+}
+
+function bikeFit() {
+  window.open("/Pages/Bike Fit.html")
+}
